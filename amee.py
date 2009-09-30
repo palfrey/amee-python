277203950fcf10627c7a41e82cc4cd7d818db272
Tom Parker <palfrey@tevp.net>
2009-09-30 01:33:31 +0100

    Reformat DataCategory as a dictionary

--- amee.py
+++ amee.py
@@ -1,6 +1,7 @@
 from urlgrab.URLTimeout import URLTimeoutError
 from urlgrab.GetURL import GetURL
 from xml.etree.ElementTree import fromstring, ElementTree
+from types import ListType
 
 class AMEE:
 	def __init__(self, server, username = None, password = None):
@@ -33,13 +34,38 @@ class AMEE:
 			else:
 				raise
 		xml = fromstring(data)
-		return ElementTree(xml)
+		return ElementTree(xml).getroot()
 
 	def DataCategory(self, path = None):
 		if path == None:
-			return self._get_authed("data")
+			return DataCategory(self._get_authed("data"))
 		else:
-			return self._get_authed("data/%s"%path)
+			return DataCategory(self._get_authed("data/%s"%path))
+
+class XMLDictionary(dict):
+	def __init__(self, top):
+		self.tag = top.tag[top.tag.find("}")+1:]
+		for (k,v) in top.items():
+			self[k] = v
+		for k in top.getchildren():
+			c = XMLDictionary(k)
+			if k.text!=None:
+				assert c.keys() == [],(k.text,c.keys())
+				assert not self.has_key(c.tag)
+				self[c.tag] = k.text
+			if len(c.keys()) == 0:
+				continue
+			if self.has_key(c.tag):
+				orig = self[c.tag]
+				if type(orig) != ListType:
+					orig = [orig]
+				orig.append(c)
+				self[c.tag] = orig
+			else:
+				self[c.tag] = c
+
+class DataCategory(XMLDictionary):
+	pass
 	
 if __name__ == "__main__":
 	(u,p) = [x.strip() for x in open("config").readlines() if x.strip()!=""]
